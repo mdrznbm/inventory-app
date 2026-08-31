6afa8e337ba1e58e15a9f0b900d07a6b54e83857
kttpckp <fittrat22@gmail.com>
2026-08-31 10:43:15 +0800

    Move secret key to .env

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import re
+from dotenv import load_dotenv
 from datetime import datetime
 from io import BytesIO
 from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, send_file
@@ -9,9 +10,11 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill, Alignment
 from models import db, User, Product, Transaction
 
+load_dotenv(".env")
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'dev-secret-key-change-in-production'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'inventory.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
